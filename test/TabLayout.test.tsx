72c88c7460a96db9609a69d3cf65c8f3b2c08a57
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2023-02-23 14:09:16 -0300

    test: adding style tests

--- test/TabLayout.test.tsx
+++ test/TabLayout.test.tsx
@@ -90,7 +90,7 @@ describe('TabLayout', () => {
     expect(tab3[0].classList.contains(CLASS)).toBe(false)
   })
 
-  it('should change selected tab', () => {
+  it('should change to tab 2', () => {
     render(
       <TabLayout>
         <Tab label="Tab 1">
@@ -116,5 +116,311 @@ describe('TabLayout', () => {
     )
 
     expect(screen.getByText('Tab 2 content')).toBeDefined()
+  })
+
+  it('should change to tab 3', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const TAB_3 = screen.getByText('Tab 3')
+
+    fireEvent(
+      TAB_3,
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+
+    expect(screen.getByText('Tab 3 content')).toBeDefined()
+  })
+
+  it('should render tab content correctly', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__content')
+
+    expect(sut).toBeDefined()
+  })
+
+  it('should render tab content with correct padding', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__content')
+
+    expect(sut).toHaveStyle(`padding: 1.2em`)
+  })
+
+  it('should render tab content with correct font-size', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__content')
+
+    expect(sut).toHaveStyle(`font-size: 1em`)
+  })
+
+  it('should render tabs with correct list-style', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__tabs')
+
+    expect(sut).toHaveStyle(`list-style: none`)
+  })
+
+  it('should render tabs with correct display infos', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__tabs')
+
+    expect(sut).toHaveStyle(`display: flex`)
+    expect(sut).toHaveStyle(`align-items: center`)
+    expect(sut).toHaveStyle(`justify-content: center`)
+  })
+
+  it('should render tab selected with correct style', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__tab--selected')
+
+    expect(sut).toHaveStyle(`border-bottom: solid 1px #000`)
+  })
+
+  it('should render tab with correct cursor style', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__tab')
+
+    expect(sut).toHaveStyle(`cursor: pointer`)
+  })
+
+  it('should render tab with correct margin', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__tab')
+
+    expect(sut).toHaveStyle(`margin-right: 0.5em`)
+  })
+
+  it('should render tab content with correct font-family', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelector('.tab-layout__content')
+
+    expect(sut).toHaveStyle(`font-family: sans-serif`)
+  })
+
+  it('should render 3 tabs', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <p>Tab 1 content</p>
+        </Tab>
+        <Tab label="Tab 2">
+          <p>Tab 2 content</p>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const sut = document.querySelectorAll('.tab-layout__tab')
+
+    expect(sut.length).toEqual(3)
+  })
+
+  it('should render main tag', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <main>Tab 1 content</main>
+        </Tab>
+        <Tab label="Tab 2">
+          <section>Tab 2 content</section>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const main = document.getElementsByTagName('main')
+
+    expect(main.length).toEqual(1)
+  })
+
+  it('should render section tag', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <main>Tab 1 content</main>
+        </Tab>
+        <Tab label="Tab 2">
+          <section>Tab 2 content</section>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const section = document.getElementsByTagName('section')
+
+    fireEvent(
+      screen.getByText('Tab 2'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+
+    expect(section.length).toEqual(1)
+  })
+
+  it('should render p tag', () => {
+    render(
+      <TabLayout>
+        <Tab label="Tab 1">
+          <main>Tab 1 content</main>
+        </Tab>
+        <Tab label="Tab 2">
+          <section>Tab 2 content</section>
+        </Tab>
+        <Tab label="Tab 3">
+          <p>Tab 3 content</p>
+        </Tab>
+      </TabLayout>
+    )
+
+    const p = document.getElementsByTagName('p')
+
+    fireEvent(
+      screen.getByText('Tab 3'),
+      new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+      })
+    )
+
+    expect(p.length).toEqual(1)
   })
 })
